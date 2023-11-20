c793ef97e7d869cb284e79e3890eedc8777d4e7c
marquavious <marq.draggon@gmail.com>
2023-11-20 13:07:23 -0500

    Clean Up

--- SwfitUI Connect And Care/ContentView.swift
+++ SwfitUI Connect And Care/ContentView.swift
@@ -16,7 +16,6 @@ struct ContentView: View {
                 TabView {
                     ForEach(0...4, id: \.self) { _ in
                         VStack(spacing: 20) {
-                            Spacer()
                             Rectangle()
                                 .fill(Color.red)
                                 .clipped()
@@ -25,17 +24,11 @@ struct ContentView: View {
                                 .foregroundStyle(Color.white)
                                 .fontWeight(.semibold)
                                 .font(.system(size: 26))
+                                .multilineTextAlignment(.center)
                             Text("Welcome to Connect & Care: Let's change philanthropy together")
                                 .foregroundStyle(Color.white)
-//                            Spacer()
-
+                                .multilineTextAlignment(.center)
                         }
-//                        .frame(maxHeight: .infinity, alignment: .bottom)
-                        /*
-                        Rectangle()
-                            .fill(Color.red)
-                            .clipped()
-                         */
                     }
                 }
                 .frame(maxHeight: .infinity, alignment: .bottom)
@@ -65,45 +58,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-
-//Text("Welcome!")
-//Text("Welcome to Connect & Care: Let's change philanthropy together")
-
-
-/*
-ZStack {
-    Color(red: 28/255, green: 68/255, blue: 108/255)
-        .ignoresSafeArea()
-    VStack {
-        ScrollView([.horizontal], showsIndicators: true) {
-            VStack() {
-                HStack(spacing: 0) {
-                    ForEach(0...4, id: \.self) { _ in
-                        Rectangle()
-                            .fill(Color.red)
-                            .frame(width: UIScreen.main.bounds.width, height: .infinity)
-                            .clipped()
-                    }
-                }
-            }
-        }
-        .scrollTargetBehavior(.paging)
-
-        VStack {
-            Button("Continue with Facebook") { }
-                .frame(maxWidth: .infinity, minHeight:47)
-                .background(Color.white)
-                .foregroundColor(Color(red: 22/255, green: 51/255, blue: 89/255))
-                .fontWeight(.semibold)
-                .cornerRadius(3.0)
-            Button("Continue with Email") { }
-                .frame(maxWidth: .infinity, minHeight:47)
-                .background(Color.white.opacity(0.08))
-                .foregroundColor(.white)
-                .fontWeight(.semibold)
-                .cornerRadius(3.0)
-        }
-        .padding(EdgeInsets(top: 0, leading: 16, bottom: 20, trailing: 16))
-    }
-}
-*/
